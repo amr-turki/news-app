f1fb5d223a32e3d5d5789ac9ed23ba78759c8bf7
Amr Turki <amrturki79@gmail.com>
2026-04-25 21:17:25 +0300

    create category widget

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:news_application/models/category_model.dart';
+
+class Category extends StatelessWidget {
+  const Category({super.key, required this.model});
+
+  final CategoryModel model;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 15.0),
+      child: Container(
+        height: 75,
+        width: 180,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(model.image),
+          ),
+        ),
+        child: Center(
+          child: Text(
+            model.text,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 14,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
